665dbb6080eaa8f79e0b787d7d1fefa7427c1410
Barry Lin <39817188+Barry028@users.noreply.github.com>
2021-11-02 20:59:10 +0800

    no message

--- js/components/loading.js
+++ js/components/loading.js
@@ -1,18 +1,46 @@
 var loadingEl =
-  '<div id="logo_loader">\
-  <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">\
-      <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>\
-  </svg>\
+'<div id="logo_loader">\
+	<div class="loader-cnt">\
+		<div class="loader">\
+		    <svg viewBox="0 0 80 80">\
+		        <circle id="test" cx="40" cy="40" r="32"></circle>\
+		    </svg>\
+		</div>\
+		<div class="loader triangle">\
+		    <svg viewBox="0 0 86 80">\
+		        <polygon points="43 8 79 72 7 72"></polygon>\
+		    </svg>\
+		</div>\
+		<div class="loader">\
+		    <svg viewBox="0 0 80 80">\
+		        <rect x="8" y="8" width="64" height="64"></rect>\
+		    </svg>\
+		</div>\
+	</div>\
 </div>';
 
 document.write(loadingEl);
 document.onreadystatechange = completeLoading;
 
 function completeLoading() {
-  if (document.readyState == 'complete') {
-
-    var loadingMask = document.getElementById('logo_loader');
-    loadingMask.parentNode.removeChild(loadingMask);
-
-  }
+	switch (document.readyState) {
+	  case "loading":
+	    // The document is still loading.
+	    break;
+	  case "interactive":
+	    // The document has finished loading. We can now access the DOM elements.
+	    // But sub-resources such as scripts, images, stylesheets and frames are still loading.
+		// const span = document.createElement("span");
+		// span.textContent = "A <span> element.";
+		// document.body.appendChild(span);
+	    break;
+	  case "complete":
+	    var loadingMask = document.getElementById('logo_loader');
+    	loadingMask.parentNode.removeChild(loadingMask);
+    	
+	    break;
+	}
 }
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,42 +1,69 @@
         ;
         (function($) {
 
-          $.BcMain = {
+          $.Main = {
 
             init: function() {
 
               $(document).ready(function(e) {
 
-                // Extends jQuery
-                $.BcMain.helpers.extendjQuery();
                 // Botostrap Tootltips
                 $('[data-toggle="tooltip"]').tooltip();
+
                 // Botostrap Popover
                 $('[data-toggle="popover"]').popover();
 
+                // Extends jQuery
+                $.Main.helpers.extendjQuery();
+
+                // filterList
+                $.Main.components.filterList();
+
                 // Set Background Image Dynamically
-                if ($('[data-bg-img-src]').length) $.BcMain.helpers.bgImage($('[data-bg-img-src]'));
+                if ($('[data-bg-img-src]').length)
+                  $.Main.helpers.bgImage($('[data-bg-img-src]'));
 
                 // Detect Internet Explorer (IE)
-                $.BcMain.helpers.detectIE();
+                $.Main.helpers.detectIE();
 
-                $.BcBackToGo.init('.js-go-to');
+                // Go Top
+                $.BackToGo.init('.js-go-to');
 
-                // initialization of horizontal progress bars
-                var horizontalProgressBars = $.BcProgressBar.init('.js-hr-progress-bar', {
-                  direction: 'horizontal',
-                  indicatorSelector: '.js-hr-progress-bar-indicator'
+
+                var link = $('#redeMenu li a');
+                // Move to specific section when click on menu link
+                link.on('click', function(e) {
+                  var target = $($(this).attr('href'));
+                  $('html, body').animate({
+                    scrollTop: target.offset().top
+                  }, 200);
+                  $(this).addClass('active');
+                  e.preventDefault();
                 });
 
+                // Run the scrNav when scroll
+                $(window).on('scroll', function() {
+                  scrNav();
+                });
+
+                // scrNav function 
+                // Change active dot according to the active section in the window
+                function scrNav() {
+                  var sTop = $(window).scrollTop();
+                  $('.scroll_menu').each(function() {
+                    var id = $(this).attr('id'),
+                      offset = $(this).offset().top - 160 ,
+                      height = $(this).height();
+                    if (sTop >= offset && sTop < offset + height) {
+                      link.removeClass('active');
+                      $('#redeMenu').find('[data-scroll="' + id + '"]').addClass('active');
+                    }
+                  });
+                }
+                scrNav();
               });
 
               $(window).on('load', function(e) {
-
-                scrollSpy('#redeMenu', {
-                  sectionClass: '.scrollspy',
-                  menuActiveTarget: '#redeMenu li a',
-                  offset: 200
-                });
 
                 $('#reAside').mCustomScrollbar({
                   axis: "y"
@@ -57,7 +84,7 @@
                   },
                   load: {
                     filter: 'all' // show app tab on first load
-                  }
+                  },
                 });
               },
 
@@ -183,93 +210,7 @@
 
           };
 
-          $.BcMain.init();
+          $.Main.init();
 
-          $.BcMain.components.filterList();
-
-          $('#recruit').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/recruit-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#membercenter').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/membercenter-f1.png" alt="membercenter"><img class="img-responsive" src="dist/img/portfollo/membercenter-f3.png" alt="membercenter"><img class="img-responsive" src="dist/img/portfollo/membercenter-f2.png" alt="membercenter">',
-              showConfirmButton: false,
-            })
-          });
-          $('#vacancy').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/vacancy-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#hr').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/hr-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#kokokara').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/kokokara-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#twkc').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/twkc-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#taea').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/taea-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#tscm').on('click', function() {
-            Swal.fire({
-              imageUrl: 'dist/img/portfollo/tcsm-f.png',
-              showConfirmButton: false,
-            })
-          });
-          $('#ccu').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/ccu-f.png" alt="國立中正大學"><img class="img-responsive" src="dist/img/portfollo/ccu-1-f.png" alt="國立中正大學">',
-              showConfirmButton: false,
-            })
-          });
-          $('#123share').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/123Share-1.png" alt="123 SHare App ReDesign"><img class="img-responsive" src="dist/img/portfollo/123Share-2.png" alt="123 SHare App ReDesign"><img class="img-responsive" src="dist/img/portfollo/123Share-3.png" alt="123 SHare App ReDesign">',
-              showConfirmButton: false,
-            })
-          });
-          $('#coupon').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/ifansCouPon-1.png" alt="瘋折扣 App ReDesign"><img class="img-responsive" src="dist/img/portfollo/ifansCouPon-2.png" alt="瘋折扣 App ReDesign"><img class="img-responsive" src="dist/img/portfollo/ifansCouPon-3.png" alt="瘋折扣 App ReDesign">',
-              showConfirmButton: false,
-            })
-          });
-          $('#market').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/market-1.png" alt="瘋市場 App ReDesign"><img class="img-responsive" src="dist/img/portfollo/market-2.png" alt="瘋市場 App ReDesign"><img class="img-responsive" src="dist/img/portfollo/market-3.png" alt="瘋市場 App ReDesign">',
-              showConfirmButton: false,
-            })
-          });
-          $('#myweb').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/MyWeb-1.png" alt="MyWeb App ReDesign"><img class="img-responsive" src="dist/img/portfollo/MyWeb-2.png" alt="MyWeb App ReDesign"><img class="img-responsive" src="dist/img/portfollo/MyWeb-3.png" alt="MyWeb App ReDesign">',
-              showConfirmButton: false,
-            })
-          });
-          $('#onefix').on('click', function() {
-            Swal.fire({
-              html: '<img class="img-responsive" src="dist/img/portfollo/onefixCIS-01.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-02.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-03.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-04.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-05.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-06.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-07.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-08.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-09.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-10.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-11.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-12.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-13.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-14.png" alt="onefixCIS"><img class="img-responsive" src="dist/img/portfollo/onefixCIS-15.png" alt="onefixCIS">',
-              showConfirmButton: false,
-            })
-          });
 
         })(jQuery);
